f9c0db0a91a7add774f1a1f613967391a0118b6b
kayraozdemir6767-sketch <kayraozdemir6767@gmail.com>
2026-04-09 20:40:36 +0300

    Update mount.sh

--- mount.sh
+++ mount.sh
@@ -1,7 +1,10 @@
-mount --types proc $LINUX/proc
+mount --types proc /proc $LINUX/proc
 mount --rbind /sys $LINUX/sys
 mount --make-rslave $LINUX/sys
 mount --rbind /dev $LINUX/dev
 mount --make-rslave $LINUX/dev
 mount --bind /run $LINUX/run
 mount --make-slave $LINUX/run
+test -L /dev/shm && rm /dev/shm && mkdir /dev/shm
+mount --types tmpfs --options nosuid,nodev,noexec shm /dev/shm
+chmod 1777 /dev/shm /run/shm
